13e9e91e861ec3dcbc0c8b0002f120a92a726452
Armando Gonzalez <armando.gonzalez@correo.unimet.edu.ve>
2023-03-24 15:04:31 -0600

    landing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,29 @@ import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
 
 const Landing = () => {
-  return <h4>Landing Page</h4>
+  return <main>
+    <nav>
+        <img src={logo} alt="jobify" className='logo' />
+    </nav>
+    <div className='container page'>
+        {/* info */}
+        <div className='info'>
+            <h1>
+                job <span>tracking</span> app 
+            </h1>
+            <p>
+                I'm baby photo booth crucifix semiotics, blog sustainable grailed blue bottle 
+                fashion axe godard mixtape farm-to-table jianbing woke pour-over.
+                JOMO disrupt humblebrag sus, pug whatever flannel hot chicken hashtag 90's bicycle 
+                rights yuccie meh banjo kale chips. Tumeric paleo vape, taiyaki aesthetic sus 
+                gluten-free pok pok cronut occupy yes plz. Helvetica solarpunk selfies, 
+                mustache normcore PBR&B neutral milk hotel art party single-origin coffee godard.
+            </p>
+            <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+    </div>
+  </main>
 }
 
 export default Landing
