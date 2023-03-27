096889b9a391b86d53b15fb6dc381c46637750b0
Armando Gonzalez <armando.gonzalez@correo.unimet.edu.ve>
2023-03-27 14:05:42 -0600

    Routes

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
 import {Logo} from '../components'
+import {Link} from 'react-router-dom'
 
 const Landing = () => {
   return( 
@@ -22,7 +23,7 @@ const Landing = () => {
                     gluten-free pok pok cronut occupy yes plz. Helvetica solarpunk selfies, 
                     mustache normcore PBR&B neutral milk hotel art party single-origin coffee godard.
                 </p>
-                <button className='btn btn-hero'>Login/Register</button>
+                <Link to='/register' className='btn btn-hero'>Login/Register</Link>
             </div>
             <img src={main} alt='job hunt' className='img main-img' />
         </div>
