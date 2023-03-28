29acfa7fec1904e2cad3db9090b2c57a72d34be3
Armando Gonzalez <armando.gonzalez@correo.unimet.edu.ve>
2023-03-28 15:26:11 -0600

    DisplayAlert setup

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -2,6 +2,8 @@ import React, {useReducer, useContext } from 'react';
 
 import reducer from './reducer';
 
+import { DISPLAY_ALERT } from "./actions";
+
 const initialState = {
     isLoading: false,
     showAlert: false,
@@ -13,7 +15,12 @@ const AppContext = React.createContext();
 
 const AppProvider = ({children}) =>{
     const [state, dispatch] = useReducer(reducer, initialState); // TODO: INVESTIGATE THIS LINE
-    return (<AppContext.Provider value={{...state}}>
+    
+    const displayAlert = () =>{
+        dispatch({type:DISPLAY_ALERT}); // TODO: INVESTIGATE THIS LINE
+    };
+    
+    return (<AppContext.Provider value={{...state, displayAlert}}>
         {children}
     </AppContext.Provider>)
 };
